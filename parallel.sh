eace5e2e8b9cc29c9bda8c50439d6f00a1966850
David Bernal Neira <bernalde@euler.cheme.cmu.edu>
2019-11-04 17:10:32 -0500

    Updates on run files

---
+++ parallel.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Set the number of nodes and processes per node
+#PBS -l nodes=1:ppn=1
+
+# Set max wallclock time
+#PBS -l walltime=48:00:00
+
+# Set maximum memory
+#PBS -l mem=3gb
+
+# Set name of job
+#PBS -N random_socp.random_2_2_2_1.sbb.ipopth.0
+
+# Use submission environment
+#PBS -V
+cd /home/bernalde/Repositories/conic_disjunctive
+/home/bernalde/gams/gams28.2_linux_x64_64_sfx/gams /home/bernalde/Repositories/conic_disjunctive/instances/socp/random_2_2_2_1 MIQCP=sbb QCP=ipopth OPTFILE=0 reslim=3600 threads=1 optcr=1e-5 iterlim=1e9 LO=2 LF=random_socp.log/random_2_2_2_1.sbb.ipopth.0.log O=random_socp.log/random_2_2_2_1.sbb.ipopth.0.lst TRACE=random_socp.trc/random_2_2_2_1.sbb.ipopth.0.trc
--- run.sh
+++ run.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
-GAMS="gams"
+GAMS="/home/bernalde/gams/gams28.2_linux_x64_64_sfx/gams"
 if [ $USER == debernal ] ; then
     INSTANCEDIR=/home/debernal/Perspective/instances
+    EXPDIR=/home/bernalde/Repositories/conic_disjunctive
 else
-    INSTANCEDIR=/home/bernalde/MINLPLib/minlplib/gms
+    INSTANCEDIR=/home/bernalde/Repositories/conic_disjunctive/instances/socp
+    EXPDIR=/home/bernalde/Repositories/conic_disjunctive
 fi
-TESTSET="convex"
-SKIPEXISTING=1   # whether to skip runs for which a trace file already exists
+TESTSET="random_socp"
+SKIPEXISTING=0   # whether to skip runs for which a trace file already exists
 
-GAMSOPTS="reslim=100 threads=1"
+GAMSOPTS="reslim=3600 threads=1 optcr=1e-5 iterlim=1e9 LO=3"
 # TODO memlimit?
 
 INSTANCES=`cut -d" " -f1 ${TESTSET}.txt`
@@ -17,21 +19,23 @@ INSTANCES=`cut -d" " -f1 ${TESTSET}.txt`
 # run a solver/option-file combination on a particular instance
 # $1 = instance
 # $2 = solvername
-# $3 = option file number
+# $3 = continuous subsolvername
+# $4 = option file number
+# $5 = binary option if NLP (0) of QCP (1)
 function runinstance ()
 {
-   TRACEFILE=${TESTSET}.trc/$1.$2.$3.trc
-   
+   TRACEFILE=${TESTSET}.trc/$1.$2.$3.$4.trc
+
    if [ $SKIPEXISTING == 1 ] && [ -e $TRACEFILE ] ; then
-      echo "Skip solver $2 with option file $3 on instance $1"
+      echo "Skip solver $2 subsolver $3 with option file $4 on instance $1"
       return
    fi
 
-   echo "Run solver $2 with option file $3 on instance $1"
+   echo "Run solver $2 subsolver $3 with option file $4 on instance $1"
 
    mkdir -p ${TESTSET}.trc
    mkdir -p ${TESTSET}.log
-   
+
    # initialize trace files
    cat > $TRACEFILE <<EOF
 * Trace Record Definition
@@ -40,28 +44,34 @@ function runinstance ()
 * ModelStatus,SolverStatus,ObjectiveValue,ObjectiveValueEstimate,SolverTime,ETSolver,NumberOfIterations,NumberOfNodes
 EOF
 
-   
-   if [ $USER == bernalde ] ; then
-      $GAMS ${INSTANCEDIR}/$1 MINLP=$2 MIQCP=$2 OPTFILE=$3 $GAMSOPTS LF=${TESTSET}.log/$1.$2.$3.log O=${TESTSET}.log/$1.$2.$3.lst TRACE=$TRACEFILE
-   
-      # change name of gams.py into actual instance name .py
-      mv gams.py ./models/$1.py
 
-   else
-       
+   if [ $USER == bernalde ] ; then
+      sed -i '13s/.*/#PBS -N '"${TESTSET}.$1.$2.$3.$4"'/' parallel.sh
+      sed -i '17s@.*@cd '"${EXPDIR}"'@' parallel.sh
+      # qsub parallel.sh
+      if [ $5 == 0 ]; then
+          $GAMS ${INSTANCEDIR}/$1 MINLP=$2 NLP=$3 OPTFILE=$4 $GAMSOPTS LF=${TESTSET}.log/$1.$2.$3.$4.log O=${TESTSET}.log/$1.$2.$3.$4.lst TRACE=$TRACEFILE
+          sed -i '18s@.*@'"$GAMS ${INSTANCEDIR}/$1 MINLP=$2 NLP=$3 OPTFILE=$4 $GAMSOPTS LF=${TESTSET}.log/$1.$2.$3.$4.log O=${TESTSET}.log/$1.$2.$3.$4.lst TRACE=$TRACEFILE"'@' parallel.sh
+      else
+          $GAMS ${INSTANCEDIR}/$1 MIQCP=$2 QCP=$3 OPTFILE=$4 $GAMSOPTS LF=${TESTSET}.log/$1.$2.$3.$4.log O=${TESTSET}.log/$1.$2.$3.$4.lst TRACE=$TRACEFILE
+          sed -i '18s@.*@'"$GAMS ${INSTANCEDIR}/$1 MIQCP=$2 QCP=$3 OPTFILE=$4 $GAMSOPTS LF=${TESTSET}.log/$1.$2.$3.$4.log O=${TESTSET}.log/$1.$2.$3.$4.lst TRACE=$TRACEFILE"'@' parallel.sh
+
+      fi
    fi
 }
 
 # run a solver/option-file combination on the set of instances
 # $1 = solvername
-# $2 = option file number
+# $2 = continuous subsolvername
+# $3 = option file number
 function runsolveropt ()
 {
    for i in $INSTANCES
    do
-      runinstance $i $1 $2
+      runinstance $i $1 $2 $3
    done
 }
 
 # run all
-runsolveropt convert 1
+runsolveropt sbb ipopth 0 1
+#runsolveropt sbb ipopth 0 0
