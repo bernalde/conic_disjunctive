b65f02a343bbe04521f459967fe8cd38791a755e
David Bernal <bernalde@cmu.edu>
2020-03-17 17:27:47 -0400

    Adding paver codes to generate results

---
+++ paver/dorsyn.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+# use 3600s if solver failed
+# reduce mintime to 0.1s, which fits better to local solvers
+# disable examiner checks for feasibility and optimality
+python3 ../src/paver/paver.py \
+  rsyn_full_trc/*.trc \
+  --failtime 3600 \
+  --mintime 0.1 \
+  --ccopttol inf \
+  --ccfeastol inf \
+  --writetext rsyn.txt \
+  --ignoredualbounds
+#  --writehtml rsyn.html \
