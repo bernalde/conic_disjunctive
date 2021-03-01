4d6786b2187ae1520bf808abfdfa54586a8126db
David Bernal <bernalde@cmu.edu>
2021-03-01 17:28:49 -0500

    Adding logistic results

--- parallel.sh
+++ parallel.sh
@@ -10,9 +10,9 @@
 #PBS -l mem=3gb
 
 # Set name of job
-#PBS -N random_socp.random_5_5_5_HRC_9.gms.cplex.cplex.0
+#PBS -N logistic.logistic_5_20_3_HRE_9.gms.knitro.knitro.0
 
 # Use submission environment
 #PBS -V
 cd /home/bernalde/Repositories/conic_disjunctive
-/home/bernalde/gams/gams28.2_linux_x64_64_sfx/gams /home/bernalde/Repositories/conic_disjunctive/instances/socp/random_5_5_5_HRC_9.gms MIQCP=cplex QCP=cplex OPTFILE=0 reslim=3600 threads=1 optcr=1e-5 iterlim=1e9 LO=3 nodlim=1000000 LF=random_socp.log/random_5_5_5_HRC_9.gms.cplex.cplex.0.log O=random_socp.log/random_5_5_5_HRC_9.gms.cplex.cplex.0.lst TRACE=random_socp.trc/random_5_5_5_HRC_9.gms.cplex.cplex.0.trc --TYPE=MIQCP
+/home/bernalde/gams/gams28.2_linux_x64_64_sfx/gams /home/bernalde/Repositories/conic_disjunctive/instances/logistic/logistic_5_20_3_HRE_9.gms MINLP=knitro NLP=knitro OPTFILE=0 reslim=3600 threads=1 optcr=1e-5 iterlim=1e9 LO=3 nodlim=1000000 LF=logistic.log/logistic_5_20_3_HRE_9.gms.knitro.knitro.0.log O=logistic.log/logistic_5_20_3_HRE_9.gms.knitro.knitro.0.lst TRACE=logistic.trc/logistic_5_20_3_HRE_9.gms.knitro.knitro.0.trc --TYPE=MINLP
