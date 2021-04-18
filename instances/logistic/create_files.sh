28f5809ca5f6d7e62108488f91ae0c1aeb85c30f
David Bernal <bernalde@cmu.edu>
2021-04-18 04:16:29 -0500

    Fixing BMc reformulation

--- instances/logistic/create_files.sh
+++ instances/logistic/create_files.sh
@@ -1,10 +1,13 @@
+# From file ${FILENAME}"$K1"'_'"$J1"'_'"$L1"'_'"$REFOR"'_1.gms' generate other files with name
+# ${FILENAME}"$K"'_'"$J"'_'"$L"'_'"$REFOR"'_i.gms' with i from 1 to 10
+# Changing seed numer to i, K to disjunctions, J to disjunctive terms and L to variable dimension
 FILENAME="logistic_"
 K1="2"
 J1="20"
 L1="1"
-K="10"
+K="2"
 J="20"
-L="3"
+L="2"
 for REFOR in 'BM' 'BMc' 'HRS' 'HRc' 'HRE' 'HRL'
 do
   cp ${FILENAME}"$K1"'_'"$J1"'_'"$L1"'_'"$REFOR"'_1.gms' ${FILENAME}"$K"'_'"$J"'_'"$L"'_'"$REFOR"'_1.gms'
@@ -21,15 +24,3 @@ do
     echo "Created file: $i"
   done
 done
-
-
-#for D in ./*; do
-#    if [ -d "$D" ]; then
-#	if [[ $iter -gt 5 && $iter -lt 12 ]]; then
-#	echo $iter
-#	sed -i '13s/.*/#PBS -N contracts_'"$iter"'/' parallel.sh
-#	sed -i '17s@.*@cd /home/bernalde/IntroToMLProj/GDP_Refomulation_NN/data/Logic_based_B_B_contracts/Euler/'"$iter"'@' parallel.sh
-#	fi
-#	iter=$(( $iter + 1))
-#    fi
-#done
